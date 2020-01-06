fe95ed66de7aeedbbbd8ad69d3409b6586ff9fc3
Khoa <knguyen@captechconsulting.com>
2020-01-06 15:38:14 -0500

    add additional parallax photo sections

---
+++ src/components/BackgroundSectionThree.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+import styled from 'styled-components'
+
+import BackgroundImage from 'gatsby-background-image'
+const BackgroundSectionThree = ({ className }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "shadowcreek.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      // Set ImageData.
+      const imageData = data.desktop.childImageSharp.fluid
+      return (
+        <BackgroundImage
+          Tag="section"
+          className={className}
+          fluid={imageData}
+          backgroundColor={`#040e18`}
+        >
+        </BackgroundImage>
+      )
+    }}
+  />
+)
+
+const StyledBackgroundSectionThree = styled(BackgroundSectionThree)`
+  width: 100%;
+  height: 500px;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
+
+export default StyledBackgroundSectionThree
