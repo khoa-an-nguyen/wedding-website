548cdf72d8ede7c345bef565cc2a1dfa611a5a64
Khoa <knguyen@captechconsulting.com>
2020-01-16 00:19:35 -0500

    add calendar button to intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { graphql, StaticQuery } from 'gatsby'
 import styled from 'styled-components'
-
 import BackgroundImage from 'gatsby-background-image'
-const ImageText = styled.h1`
+import AddToCalendar from 'react-add-to-calendar'
 
-`;
+const event = {
+  title: 'My-Anh and Khoas Wedding!',
+  description: 'Come celebrate with us on this special day! Please visit our website for further details on this day!',
+  location: '18090 Silcott Springs Rd, Purcellville, VA 20132',
+  startTime: '2020-10-02T22:00:00-00:00',
+  endTime: '2020-10-02T03:00:00-00:00'
+}
+let icon = { 'calendar-plus-o': 'left' };
+
 
 const Intro = ({ className }) => (
   <StaticQuery
@@ -36,7 +43,10 @@ const Intro = ({ className }) => (
           >
             <div>
               <h1 id="hero-text" class="main-image-text">My-Anh & Khoa</h1>
+              <div>
               <h2 id="date-text" class="main-image-text">Friday, October 2, 2020</h2>
+              <h5 id="calendar-wrapper"><AddToCalendar event={event} buttonLabel="Save the Date!" buttonTemplate={icon} /></h5>
+              </div>
             </div>
           </BackgroundImage>
         </section>
