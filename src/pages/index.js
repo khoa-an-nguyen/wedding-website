6fa5adfac3ba0401ca1a613aafbf5fcc6ec14baf
Khoa <knguyen@captechconsulting.com>
2020-02-06 20:36:49 -0500

    change nav link and directions

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,14 +41,17 @@ class Index extends React.Component {
         <Layout>
           <Helmet title="#TheNewLeNguyens" />
           {/* <Header /> */}
-          <Waypoint
-            onEnter={this._handleWaypointEnter}
-            onLeave={this._handleWaypointLeave}
-          ></Waypoint>
+          <section id="intro">
+            <Waypoint
+              onEnter={this._handleWaypointEnter}
+              onLeave={this._handleWaypointLeave}>
+              </Waypoint>
+          </section>
+
           <Nav sticky={this.state.stickyNav} />
           <div id="main">
-          <Intro />
-            <section id="intro" className="main special">
+              <Intro />
+            <section id="introd" className="main special">
               <div className="spotlight">
                 <div className="content">
                   <header className="major">
@@ -140,7 +143,7 @@ class Index extends React.Component {
                 <h3>Shadow Creek</h3>
                 <address>18090 Silcott Springs Road</address>
                 <address>Purcellville, Virginia 20132</address>
-                <a href="https://goo.gl/maps/wCGF24ZfsJMxDzer7" className="button">Get Directions</a>
+                <a href="https://goo.gl/maps/wCGF24ZfsJMxDzer7" className="button special">Get Directions</a>
                 <div class="travel-wrapper">
                   <div class="travel-section">
                     <div>
@@ -148,32 +151,25 @@ class Index extends React.Component {
                       <h2>Traveling from Virginia</h2>
                       <h4>Directions</h4>
                     </div>
-                    {/* <p>Toll Route:
-                      1. Begin on VA-267 W (Dulles Toll Road/Dulles Greenway) toward Leesburg
-                      2. Take Exit 1A - US-15 S/VA-7 W/Leesburg Bypass toward Leesburg/Warrenton
-                      3. After 9.3 miles, take the VA-287 S exit toward VA-7 BUS/Purcellville/Lovettsville and turn left onto VA-287 S/Berlin Turnpike
-                      4. Continue straight through the traffic circle to continue on State Rte 1610
-                      5. After 2 miles, turn left on State Rte 690/Silcott Springs Road
-                      6. After 0.7 miles, Shadow Creek will be on the left
-                    </p> */}
                     <p>Toll Route</p>
-                    <ol>
-                      <li>Begin on VA-267 W (Dulles Toll Road/Dulles Greenway) toward Leesburg</li>
-                      <li>Take Exit 1A - US-15 S/VA-7 W/Leesburg Bypass toward Leesburg/Warrenton</li>
-                      <li>After 9.3 miles, take the VA-287 S exit toward VA-7 BUS/Purcellville/Lovettsville and turn left onto VA-287 S/Berlin Turnpike</li>
-                      <li>Continue straight through the traffic circle to continue on State Rte 1610</li>
-                      <li>After 2 miles, turn left on State Rte 690/Silcott Springs Road</li>
-                      <li>After 0.7 miles, Shadow Creek will be on the left</li>
-                    </ol>
+
+                    <ul class="directions">
+                      <li>1. Begin on VA-267 W (Dulles Toll Road/Dulles Greenway) toward Leesburg</li>
+                      <li>2. Take Exit 1A - US-15 S/VA-7 W/Leesburg Bypass toward Leesburg/Warrenton</li>
+                      <li>3. After 9.3 miles, take the VA-287 S exit toward VA-7 BUS/Purcellville/Lovettsville and turn left onto VA-287 S/Berlin Turnpike</li>
+                      <li>4. Continue straight through the traffic circle to continue on State Rte 1610</li>
+                      <li>5. After 2 miles, turn left on State Rte 690/Silcott Springs Road</li>
+                      <li>6. After 0.7 miles, Shadow Creek will be on the left</li>
+                    </ul>
                     <p>Toll Free Route</p>
-                    <ol>
-                      <li>Begin on VA-7 W (Route 7 West) toward Leesburg/Winchester</li>
-                      <li>Take the exit ramp to merge onto US-15 S/VA-7 W/Leesburg Bypass </li>
-                      <li>After 9.3 miles, take the VA-287 S exit toward VA-7 BUS/Purcellville/Lovettsville and turn left onto VA-287 S/Berlin Turnpike</li>
-                      <li>Continue straight through the traffic circle to continue on State Rte 1610</li>
-                      <li>After 2 miles, turn left on State Rte 690/Silcott Springs Road</li>
-                      <li>After 0.7 miles, Shadow Creek will be on the left</li>
-                    </ol>
+                    <ul class="directions">
+                      <li>1. Begin on VA-7 W (Route 7 West) toward Leesburg/Winchester</li>
+                      <li>2. Take the exit ramp to merge onto US-15 S/VA-7 W/Leesburg Bypass</li>
+                      <li>3. After 9.3 miles, take the VA-287 S exit toward VA-7 BUS/Purcellville/Lovettsville and turn left onto VA-287 S/Berlin Turnpike</li>
+                      <li>4. Continue straight through the traffic circle to continue on State Rte 1610</li>
+                      <li>5. After 2 miles, turn left on State Rte 690/Silcott Springs Road</li>
+                      <li>6. After 0.7 miles, Shadow Creek will be on the left</li>
+                    </ul>
                   </div>
                   {/* <hr></hr> */}
                   <div class="travel-section">
@@ -182,20 +178,21 @@ class Index extends React.Component {
                       <h2>Traveling from Maryland</h2>
                       <h4>Directions</h4>
                     </div>
-                    <ol>
-                      <li>Begin on I-270 North towards Frederick</li>
-                      <li>Use the right 2 lanes to take Exit 32 to merge onto I-70 West toward Hagerstown</li>
-                      <li>Take Exit 52 to merge onto US-15 South/US-340 West</li>
-                      <li>Take the Exit for MD-17 toward Brunswick/Burkittsville and exit the traffic circle onto MD-17/Burkittsville</li>
-                      <li>Continue through each traffic circle to stay on MD-17 </li>
-                      <li>Merge right to stay on MD-17 South/Burkittsville Rd/Petersville Rd</li>
-                      <li>Stay on Burkittsville Rd through the traffic circle before the MD-VA bridge</li>
-                      <li>Cross the bridge into Virginia, where Burkittsville Rd becomes Berlin Turnpike</li>
-                      <li>Turn left on Town Center Dr to stay on Berlin Turnpike in Lovettsville</li>
-                      <li>Continue straight through the traffic circle to continue on State Rte 1610</li>
-                      <li>After 2 miles, turn left on State Rte 690/Silcott Springs Road</li>
-                      <li>After 0.7 miles, Shadow Creek will be on the left</li>
-                    </ol>
+                    <ul class="directions">
+                      <li>1. Begin on I-270 North towards Frederick</li>
+                      <li>2. Use the right 2 lanes to take Exit 32 to merge onto I-70 West toward Hagerstown</li>
+                      <li>3. Take Exit 52 to merge onto US-15 South/US-340 West</li>
+                      <li>4. Take the Exit for MD-17 toward Brunswick/Burkittsville and exit the traffic circle onto MD-17/Burkittsville</li>
+                      <li>5. Continue through each traffic circle to stay on MD-17 </li>
+                      <li>6. Merge right to stay on MD-17 South/Burkittsville Rd/Petersville Rd</li>
+                      <li>7. Stay on Burkittsville Rd through the traffic circle before the MD-VA bridge</li>
+                      <li>8. Cross the bridge into Virginia, where Burkittsville Rd becomes Berlin Turnpike</li>
+                      <li>9. Turn left on Town Center Dr to stay on Berlin Turnpike in Lovettsville</li>
+                      <li>10. Continue straight through the traffic circle to continue on State Rte 1610</li>
+                      <li>11. After 2 miles, turn left on State Rte 690/Silcott Springs Road</li>
+                      <li>12. After 0.7 miles, Shadow Creek will be on the left</li>
+
+                    </ul>
                   </div>
                 </div>
               </div>
