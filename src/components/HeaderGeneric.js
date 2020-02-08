9fe78973d6e23ff2f0d996ea397a683b85398938
Khoa <knguyen@captechconsulting.com>
2020-02-08 12:56:31 -0500

    wording of wedding section

--- src/components/HeaderGeneric.js
+++ src/components/HeaderGeneric.js
@@ -3,7 +3,7 @@ import React from 'react'
 const HeaderGeneric = (props) => (
     <header id="header">
         <h1>The Wedding Party</h1>
-        {/* <p>Ipsum dolor sit amet nullam</p> */}
+        <p>Meet our friends and family who will be walking down the aisle with us!</p>
     </header>
 )
 
