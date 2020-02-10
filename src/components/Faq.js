503caf392dcf31da83b13841090f2a79a7088cbb
Khoa <knguyen@captechconsulting.com>
2020-02-10 15:44:49 -0500

    add additional question

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -2,24 +2,28 @@ import React, { Component } from 'react';
 import Faq from 'react-faq-component';
  
 const data = {
-  title: "Question Not Listed? Please Contact Us!",
+  title: "Questions Not Listed? Please Contact Us!",
   rows: [
-    {
-      title: "Can I bring a plus one?",
-      content: `Per the guest limit at our venue, we will we not be able to accommodate special requests for additional guests to our wedding. Please follow the RSVPs since it will include the amount of people we intend to extend our invite for your family.`
-    },
     {
       title: "Is there a registry?",
       content: "We are so happy that you’ll be able to join us for our wedding! Your presence at this special day alone is the greatest gift of all. As many of you know, we are currently saving up for our future home and plan on starting this next step in our journey after our wedding. However, we are setting up a website to fund our honeymoon and future necessities that we will be purchasing for our future homes.. so no need to haul a gift!"
     },
     {
+      title: "Can I bring a plus one?",
+      content: `Per the guest limit at our venue, we will we not be able to accommodate special requests for additional guests to our wedding. Please follow the RSVPs since it will include the amount of people we intend to extend our invite for your family.`
+    },
+    {
       title: "What is the dress code?",
-      content: `Formal attire! However, as the long goes on and the lights start to dim feel free to loosen those buttons and lose those ties/heels!`
+      content: `Cocktail attire! The ceremony will take place outside behind the venue, and there will be a grassy area with a fire pit and where games will be available. However, as the long goes on and the lights start to dim feel free to loosen those buttons and lose those ties/heels!`
     },
     {
       title: "Transportation to Hotel?",
       content: "We are currently figuring out transporation once we have an accurate count on bookings / what hotels are plan on room blocking!"
     },
+    {
+      title: "Is there parking?",
+      content: "The venue will have plenty of parking if you are planning on driving!"
+    }
   ]
 }
  
@@ -29,6 +33,7 @@ const styles = {
   rowTitleColor: 'black',
   rowContentColor: 'grey'
 }
+
 const Faqs = () => (
     <div>
         <Faq data={data} styles={styles}/>
