6a425189fe3fc3c83a333b59cd8fe631ed967649
Khoa <knguyen@captechconsulting.com>
2020-02-10 11:57:35 -0500

    party section hover and changing variable name

--- src/pages/wedding-party.js
+++ src/pages/wedding-party.js
@@ -38,56 +38,56 @@ class Generic extends React.Component {
                 <div className="wedding-party-wrapper-page">
                   <h3 class="cursive-text party-text">Team Bride</h3>
                   <ul className="wedding-party">
-                  <li className="groom">
+                  <li className="party-member">
                       <span className="icon party"><img class="party-picture" src={bride1} height="80" weight="80"></img></span>
                       <div>
                         <h3>My-Lien Bourdon</h3>
                         <h5>Matron of Honor</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={bride2} height="80" weight="80"></img></span>
                       <div>
                         <h3>Stacy Learn</h3>
                         <h5>Maid of Honor</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={bride3} height="80" weight="80"></img></span>
                       <div>
                         <h3>Evita Le</h3>
                         <h5>Bridesmaid</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={bride4} height="80" weight="80"></img></span>
                       <div>
                         <h3>Natalie Ngu</h3>
                         <h5>Bridesmaid</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={bride5} height="80" weight="80"></img></span>
                       <div>
                         <h3>Phuong Nguyen</h3>
                         <h5>Bridesmaid</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={bride6} height="80" weight="80"></img></span>
                       <div>
                         <h3>Samantha Phun</h3>
                         <h5>Bridesmaid</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={bride7} height="80" weight="80"></img></span>
                       <div>
                         <h3>Priscillia Pham</h3>
                         <h5>Bridesmaid</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={bride8} height="80" weight="80"></img></span>
                       <div>
                         <h3>Vi Lam</h3>
@@ -99,56 +99,56 @@ class Generic extends React.Component {
                 <div className="wedding-party-wrapper-page">
                 <h3 class="cursive-text party-text">Team Groom</h3>
                   <ul className="wedding-party">
-                  <li className="groom">
+                  <li className="party-member">
                   <span className="icon party"><img class="party-picture" src={groom1} height="80" weight="80"></img></span>
                       <div>
                         <h3>Beo Nguyen</h3>
                         <h5>Best Man</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={groom2} height="80" weight="80"></img></span>
                       <div>
                         <h3>Kevin Cox</h3>
                         <h5>Best Man</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={groom3} height="80" weight="80"></img></span>
                       <div>
                         <h3>Michael Vo</h3>
                         <h5>Groomsman</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={groom4} height="80" weight="80"></img></span>
                       <div>
                         <h3>Joseph Chung</h3>
                         <h5>Groomsman</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={groom5} height="80" weight="80"></img></span>
                       <div>
                         <h3>Dara Nget</h3>
                         <h5>Groomsman</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={groom6} height="80" weight="80"></img></span>
                       <div>
                         <h3>Daniel Rillo</h3>
                         <h5>Groomsman</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={groom7} height="80" weight="80"></img></span>
                       <div>
                         <h3>Tuan Dang</h3>
                         <h5>Groomsman</h5>
                       </div>
                     </li>
-                    <li className="groom">
+                    <li className="party-member">
                     <span className="icon party"><img class="party-picture" src={groom8} height="80" weight="80"></img></span>
                       <div>
                         <h3>Justin Chaphiv</h3>
