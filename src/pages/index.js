0542a7eb7ec8ae880831af5dbef7dd0ee1451c79
Khoa <knguyen@captechconsulting.com>
2020-02-10 11:26:20 -0500

    add background image and cursive text

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,11 +57,11 @@ class Index extends React.Component {
                   <header className="major">
                     <h2>Our Story</h2>
                   </header>
-                  <h3 class="story-header">How We Met</h3>
+                  <h3 class="cursive-text">How We Met</h3>
                     <p>After 18 years of My-Anh traveling to over 7 countries, living the life of a “global nomad” (as she would say),  and well with Khoa… never straying from Northern Virginia, our paths crossed when we met in Harrisonburg, Virginia at James Madison University (<span id="dukes">Go Dukes!</span>).</p>
-                  <h3 class="story-header">Start of Being College Sweethearts</h3>
+                  <h3 class="cursive-text">Start of Being College Sweethearts</h3>
                     <p>On a sunny, breezy day of October 20th, 2012 in Downtown Charlottesville, Khoa asked My-Anh to be his girlfriend with a sharpie. napkin, and some <span>sloppy handwriting.</span></p>
-                  <h3 class="story-header">The Proposal</h3>
+                  <h3 class="cursive-text">The Proposal</h3>
                     <p>6 years, 1 month, 8 days later… Khoa finally mustered up the courage to propose to My-Anh in Sint Maarten (18°1'27.660"N 63°4'38.150”W).</p>
                   <p>We can't wait to celebrate with our friends, family, and loved ones on this next journey of our life! Let the countdown begin!</p>
                 <button class="special" id="countdown"><span class="icon fa-clock-o"></span><CountDown id="countdown"></CountDown> until the day!</button>
@@ -77,7 +77,6 @@ class Index extends React.Component {
                 <Timeline />
               </div>
             </section> */}
-            {/* <StyledBackgroundSectionTwo></StyledBackgroundSectionTwo> */}
 
             <section id="first" className="main special">
               <header className="major">
@@ -127,6 +126,7 @@ class Index extends React.Component {
                 </ul>
               </footer>
             </section>
+            <StyledBackgroundSectionTwo></StyledBackgroundSectionTwo>
 
             <section id="second" className="main special">
               <header className="major">
@@ -145,61 +145,19 @@ class Index extends React.Component {
               </header>
               <div>
                 <img src={shadowcreeklogo} alt=""></img>
-                <h3>Shadow Creek</h3>
-                <address>18090 Silcott Springs Road</address>
-                <address>Purcellville, Virginia 20132</address>
-                <a href="https://goo.gl/maps/wCGF24ZfsJMxDzer7" className="button">Get Directions</a>
-                <div class="travel-wrapper">
-                  <div class="travel-section">
-                    <div>
-                      <span class="icon fa-copy"></span>
-                      <h2>Traveling from Virginia</h2>
-                      <h4>Directions</h4>
-                    </div>
-                    <p>Toll Route</p>
-
-                    <ul class="directions">
-                      <li>1. Begin on VA-267 W (Dulles Toll Road/Dulles Greenway) toward Leesburg</li>
-                      <li>2. Take Exit 1A - US-15 S/VA-7 W/Leesburg Bypass toward Leesburg/Warrenton</li>
-                      <li>3. After 9.3 miles, take the VA-287 S exit toward VA-7 BUS/Purcellville/Lovettsville and turn left onto VA-287 S/Berlin Turnpike</li>
-                      <li>4. Continue straight through the traffic circle to continue on State Rte 1610</li>
-                      <li>5. After 2 miles, turn left on State Rte 690/Silcott Springs Road</li>
-                      <li>6. After 0.7 miles, Shadow Creek will be on the left</li>
-                    </ul>
-                    <p>Toll Free Route</p>
-                    <ul class="directions">
-                      <li>1. Begin on VA-7 W (Route 7 West) toward Leesburg/Winchester</li>
-                      <li>2. Take the exit ramp to merge onto US-15 S/VA-7 W/Leesburg Bypass</li>
-                      <li>3. After 9.3 miles, take the VA-287 S exit toward VA-7 BUS/Purcellville/Lovettsville and turn left onto VA-287 S/Berlin Turnpike</li>
-                      <li>4. Continue straight through the traffic circle to continue on State Rte 1610</li>
-                      <li>5. After 2 miles, turn left on State Rte 690/Silcott Springs Road</li>
-                      <li>6. After 0.7 miles, Shadow Creek will be on the left</li>
-                    </ul>
-                  </div>
-                  {/* <hr></hr> */}
-                  <div class="travel-section">
-                    <div>
-                      <span class="icon fa-copy"></span>
-                      <h2>Traveling from Maryland</h2>
-                      <h4>Directions</h4>
-                    </div>
-                    <ul class="directions">
-                      <li>1. Begin on I-270 North towards Frederick</li>
-                      <li>2. Use the right 2 lanes to take Exit 32 to merge onto I-70 West toward Hagerstown</li>
-                      <li>3. Take Exit 52 to merge onto US-15 South/US-340 West</li>
-                      <li>4. Take the Exit for MD-17 toward Brunswick/Burkittsville and exit the traffic circle onto MD-17/Burkittsville</li>
-                      <li>5. Continue through each traffic circle to stay on MD-17 </li>
-                      <li>6. Merge right to stay on MD-17 South/Burkittsville Rd/Petersville Rd</li>
-                      <li>7. Stay on Burkittsville Rd through the traffic circle before the MD-VA bridge</li>
-                      <li>8. Cross the bridge into Virginia, where Burkittsville Rd becomes Berlin Turnpike</li>
-                      <li>9. Turn left on Town Center Dr to stay on Berlin Turnpike in Lovettsville</li>
-                      <li>10. Continue straight through the traffic circle to continue on State Rte 1610</li>
-                      <li>11. After 2 miles, turn left on State Rte 690/Silcott Springs Road</li>
-                      <li>12. After 0.7 miles, Shadow Creek will be on the left</li>
-
-                    </ul>
-                  </div>
+                <div id="location-address">
+                  <h3>Shadow Creek</h3>
+                  <address>18090 Silcott Springs Road</address>
+                  <address>Purcellville, Virginia 20132</address>
                 </div>
+                <ul className="actions">
+                <li><a href="https://goo.gl/maps/wCGF24ZfsJMxDzer7" className="button">Map It Out!</a></li>
+                <li>
+                  <Link to="/directions" className="button">
+                      Directions
+                  </Link>
+                </li>
+                </ul>
               </div>
               <footer className="major">
               </footer>
