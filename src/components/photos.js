f6b1ae307660ed867b2f453e4c01ca47a538e429
Khoa <knguyen@captechconsulting.com>
2020-02-12 17:24:25 -0500

    photo add to section

--- src/components/photos.js
+++ src/components/photos.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Gallery from 'react-photo-gallery'
 
-import captainwhite from '../images/gallery/landscape-captain-white.jpg'
-import aodairock from '../images/gallery/V50A4004.jpg'
-import blossompic from '../images/gallery/V50A4038.jpg'
+import captainwhite from '../images/gallery/landscape-captain-white.jpg';
+import aodairock from '../images/gallery/V50A4004.jpg';
+import blossompic from '../images/gallery/V50A4038.jpg';
+import pic1 from '../images/gallery/7f66c551-6769-4423-a43b-70d313b6c023_rw_1920.jpg';
+import pic2 from '../images/gallery/e9e8c298-d568-41db-aa29-6ee6c36ccbb6_rw_1920.jpg';
+import pic3 from '../images/gallery/7e564ab5-50b0-4469-ab2d-c5fb698ba134_rw_1920.jpg';
 
 export const photos = [
     {
@@ -22,12 +25,12 @@ export const photos = [
         height: 1.2
     },
     {
-        src: captainwhite,
-        width: 4.5,
+        src: pic1,
+        width: 2,
         height: 3
       },
       {
-        src: aodairock,
+        src: pic2,
         width: 3,
         height: 2
       }
