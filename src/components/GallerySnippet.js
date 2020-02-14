e24be63b6fee0bd1619228e768e09d9fff473e53
Khoa <knguyen@captechconsulting.com>
2020-02-14 15:16:25 -0500

    add smaller image file sizes for gallery snippet

--- src/components/GallerySnippet.js
+++ src/components/GallerySnippet.js
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
 import Gallery from 'react-photo-gallery'
 
-import captainwhite from '../images/gallery/landscape-captain-white.jpg';
-import aodairock from '../images/gallery/V50A4004.jpg';
+import captainwhite from '../images/gallery/landscape-captain-white_760x500.jpg';
+import aodairock from '../images/gallery/V50A4004_760x500.jpg';
 import blossompic from '../images/gallery/V50A4038.jpg';
-import pic1 from '../images/gallery/7f66c551-6769-4423-a43b-70d313b6c023_rw_1920.jpg';
-import pic3 from '../images/gallery/7e564ab5-50b0-4469-ab2d-c5fb698ba134_rw_1920.jpg';
-
-export const query = graphql`
-  query {
-    file(relativePath: { eq: "meettheparty.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 1920) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
+import pic1 from '../images/gallery/7f66c551-6769-4423-a43b-70d313b6c023_rw_1920_500x740.jpg';
+import pic3 from '../images/gallery/7e564ab5-50b0-4469-ab2d-c5fb698ba134_rw_1920_500x740.jpg';
 
 
 export const photos = [
