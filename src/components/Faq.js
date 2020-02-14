c882410ec35b25f11ae81ff23ab0fc37b83df3d2
Khoa <knguyen@captechconsulting.com>
2020-02-14 12:40:26 -0500

    changes to faq section wording

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -6,7 +6,7 @@ const data = {
   rows: [
     {
       title: "Is there a registry?",
-      content: "We are so happy that you’ll be able to join us for our wedding! Your presence at this special day alone is the greatest gift of all. As many of you know, we are currently saving up for our future home and plan on starting this next step in our journey after our wedding. However, we are setting up a website to fund our honeymoon and future necessities that we will be purchasing for our future homes.. so no need to haul a gift!"
+      content: "We are so happy that you’ll be able to join us for our wedding! Your presence at this special day alone is the greatest gift of all. As many of you know, we are currently saving up for our future home and plan on starting this next step in our journey after our wedding. If are you so kind to give us a gift for our wedding, we request that it is not physical in nature."
     },
     {
       title: "Can I bring a plus one?",
@@ -14,7 +14,7 @@ const data = {
     },
     {
       title: "What is the dress code?",
-      content: `Cocktail attire! The ceremony will take place outside behind the venue, and there will be a grassy area with a fire pit and where games will be available. However, as the long goes on and the lights start to dim feel free to loosen those buttons and lose those ties/heels!`
+      content: `Semi-formal attire! The ceremony will take place outside behind the venue, and there will be a grassy area with a fire pit and where games will be available. However, as the night goes on and the lights start to dim feel free to loosen those buttons and lose those ties/heels!`
     },
     {
       title: "Transportation to Hotel?",
@@ -23,6 +23,10 @@ const data = {
     {
       title: "Is there parking?",
       content: "The venue will have plenty of parking if you are planning on driving!"
+    },
+    {
+      title: "Is there a room block as of now?",
+      content: "Check out accomodations section! As of now we are working on finding the best possible hotel for a room block!"
     }
   ]
 }
